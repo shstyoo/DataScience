e6f291e76e5abaff4706c47cd7be1ea90f28a9f6
shstyoo <stshryu@gmail.com>
2016-08-16 23:34:13 -0400

    started overbuff scraper

--- scrape_soup/overwatch/scrape_overbuff.py
+++ scrape_soup/overwatch/scrape_overbuff.py
@@ -7,13 +7,29 @@ import json
 import time
 import sys
 
+# Initialize hero list
 hero_list = []
 
+# Get hero list
 page = requests.get('https://www.overbuff.com/heroes')
 c = page.content
 soup = BeautifulSoup(c, 'lxml')
 
+# Sort by table
 table = soup.find_all('table')
 for item in table:
     if 'table-data' in item['class']:
-        tableValues = item.find_all('tr')
+        table_values = item.find_all('tr')
+# Find table values, and get hero names (for URL)
+for item in table_values:
+	temp_list = []
+	td = item.find_all('td')
+	for item2 in td:
+		temp_list.append(item2)
+	try:
+		temp_string = temp_list[1].a['href'].split('/')
+		hero_list.append(temp_string[2])
+	except:
+		pass
+
+
